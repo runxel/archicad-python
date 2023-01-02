ee3a64fe79a276e9abf660a4ca8ce68684278f64
Lucas Becker <runxel@users.noreply.github.com>
2023-01-02 22:35:19 +0100

    add

---
+++ code/change_zone_builtins_from_userprops.py
@@ -0,0 +1,39 @@
+# Schreibt aus zwei Benutzereigenschaften die nativen Raumnamen und -nummern.
+# Die Eigenschaften können sogar Berechnungen sein!
+# BOML | @runxel · 2022
+# v1.0
+
+from archicad import ACConnection
+
+conn = ACConnection.connect()
+assert conn
+
+acc = conn.commands
+act = conn.types
+acu = conn.utilities
+
+# Alle Räume auswählen (gibt GUIDs zurück)
+elem_all_zones = acc.GetElementsByType('Zone')
+
+# Enthält die GUID der angefragten Eigenschaft (Built-ins; non-localized)
+prop_zname = acu.GetBuiltInPropertyId('Zone_ZoneName')
+prop_znumber = acu.GetBuiltInPropertyId('Zone_ZoneNumber')
+
+# Benutzereigenschaft (localized): 'Gruppe', 'Eigenschaftsname'
+# HIER ANPASSEN AN EIGENE BEDÜRFNISSE !
+prop_pname = acu.GetUserDefinedPropertyId('Allgemeine Werte', 'Raumname')
+prop_pnumber = acu.GetUserDefinedPropertyId('Allgemeine Werte', 'Raumnummer')
+
+
+# Gibt eine Liste mit den entsprechenden Eigenschaften der Elemente zurück
+# List comprehension, ein netter Python-Trick
+propvallist_pname = [val.propertyValues[0].propertyValue.value for val in acc.GetPropertyValuesOfElements(elem_all_zones, [prop_pname])]
+propvallist_pnumber = [val.propertyValues[0].propertyValue.value for val in acc.GetPropertyValuesOfElements(elem_all_zones, [prop_pnumber])]
+
+for i, elem in enumerate(elem_all_zones):
+    new_zonename = act.ElementPropertyValue(
+                elem.elementId, prop_zname, act.NormalStringPropertyValue(propvallist_pname[i]))
+    new_zonenumber = act.ElementPropertyValue(
+                elem.elementId, prop_znumber, act.NormalStringPropertyValue(propvallist_pnumber[i]))
+    # Zurückspielen der Eigenschaften
+    acc.SetPropertyValuesOfElements([new_zonename, new_zonenumber])
